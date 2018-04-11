c6ed90095fe0508bdd6eb808f0562db6d2240dd2
Tai.DangVan <tai.dangvan@2359media.com>
2018-04-11 16:34:12 +0700

    [ADD] add Splash activity

---
+++ app/src/main/java/com/taidang/themoviedb/presentation/ui/activity/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.taidang.themoviedb.presentation.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.taidang.themoviedb.R
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+
+}
