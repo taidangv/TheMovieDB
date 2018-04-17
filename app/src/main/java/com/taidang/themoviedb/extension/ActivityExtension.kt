7f1258ab5181f5a6bca61c6d63d7d4228e4739ff
Tai.DangVan <tai.dangvan@2359media.com>
2018-04-17 13:22:01 +0700

    [ADD] main toolbar

--- app/src/main/java/com/taidang/themoviedb/extension/ActivityExtension.kt
+++ app/src/main/java/com/taidang/themoviedb/extension/ActivityExtension.kt
@@ -1,12 +1,22 @@
 package com.taidang.themoviedb.extension
 
-import android.app.Activity
+import android.support.annotation.IdRes
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import com.taidang.themoviedb.presentation.TMDBApp
 
 
-val Activity.tmdbApp: TMDBApp
-    get() = application as TMDBApp
-
 val Fragment.tmdbApp: TMDBApp
     get() = activity?.application as TMDBApp
+
+val AppCompatActivity.tmdbApp: TMDBApp
+    get() = application as TMDBApp
+
+fun AppCompatActivity.setupToolbar(@IdRes toolbarId: Int, action: Toolbar.() -> Unit): Toolbar {
+    val toolbar: Toolbar = checkNotNull(findViewById(toolbarId))
+    return toolbar.run {
+        action()
+        this
+    }
+}
