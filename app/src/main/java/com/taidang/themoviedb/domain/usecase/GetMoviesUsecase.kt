a5aaccc972079eba6a2902377a01da803618cc98
Tai.DangVan <tai.dangvan@2359media.com>
2018-04-17 17:01:36 +0700

    [UPDATE] get movies usecase

--- app/src/main/java/com/taidang/themoviedb/domain/usecase/GetMoviesUsecase.kt
+++ app/src/main/java/com/taidang/themoviedb/domain/usecase/GetMoviesUsecase.kt
@@ -2,20 +2,20 @@ package com.taidang.themoviedb.domain.usecase
 
 import com.taidang.themoviedb.domain.MovieRepository
 import com.taidang.themoviedb.domain.SchedulerFactory
-import com.taidang.themoviedb.domain.model.Movie
+import com.taidang.themoviedb.domain.model.MoviesInfo
 import io.reactivex.Single
 
 class GetMoviesUsecase(schedulerFactory: SchedulerFactory, private val movieRepository: MovieRepository)
     : BaseUsecase(schedulerFactory) {
 
-    fun getNowPlayingMovies(countryCode: String, page: Int = 1): Single<List<Movie>> {
-        return movieRepository.getNowPlayingMovies()
+    fun getNowPlayingMovies(countryCode: String, page: Int = 1): Single<MoviesInfo> {
+        return movieRepository.getNowPlayingMovies(countryCode, page)
                 .subscribeOn(schedulerFactory.workerScheduler())
                 .observeOn(schedulerFactory.callbackScheduler())
     }
 
-    fun getUpcomingMovies(countryCode: String, page: Int = 1): Single<List<Movie>> {
-        return movieRepository.getUpcomingMovies()
+    fun getUpcomingMovies(countryCode: String, page: Int = 1): Single<MoviesInfo> {
+        return movieRepository.getUpcomingMovies(countryCode, page)
                 .subscribeOn(schedulerFactory.workerScheduler())
                 .observeOn(schedulerFactory.callbackScheduler())
     }
