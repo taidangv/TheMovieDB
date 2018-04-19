ed3255105605ed0a2bcbc2ec4b2457d9e7855888
Tai.DangVan <tai.dangvan@2359media.com>
2018-04-19 19:26:20 +0700

    [ADD] page transform animation

--- app/src/main/java/com/taidang/themoviedb/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/taidang/themoviedb/presentation/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.taidang.themoviedb.R
 import com.taidang.themoviedb.presentation.adapter.MainContentPagerAdapter
+import com.taidang.themoviedb.presentation.adapter.ZoomOutPageTransformer
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.support.v4.onPageChangeListener
 
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = MainContentPagerAdapter(this, supportFragmentManager)
         vMainPager.adapter = adapter
+        vMainPager.setPageTransformer(true, ZoomOutPageTransformer())
         vMainPager.onPageChangeListener {
             onPageSelected { vToolbar.title = adapter.getPageTitle(it) }
         }
---
+++ app/src/main/java/com/taidang/themoviedb/presentation/adapter/ZoomOutPageTransformer.kt
@@ -0,0 +1,31 @@
+package com.taidang.themoviedb.presentation.adapter
+
+import android.support.v4.view.ViewPager
+import android.view.View
+
+
+class ZoomOutPageTransformer : ViewPager.PageTransformer {
+
+    companion object {
+        private const val MIN_SCALE = 0.9F
+    }
+
+    override fun transformPage(view: View, position: Float) {
+        val pageWidth = view.width
+        val pageHeight = view.height
+
+        // Modify the default slide transition to shrink the page as well
+        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+        val vertMargin = pageHeight * (1 - scaleFactor) / 2
+        val horzMargin = pageWidth * (1 - scaleFactor) / 2
+        if (position < 0) {
+            view.translationX = horzMargin - vertMargin / 2
+        } else {
+            view.translationX = -horzMargin + vertMargin / 2
+        }
+
+        // Scale the page down (between MIN_SCALE and 1)
+        view.scaleX = scaleFactor
+        view.scaleY = scaleFactor
+    }
+}
